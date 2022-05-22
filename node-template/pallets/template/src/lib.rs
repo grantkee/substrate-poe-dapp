cc8c708b75584b04bb8dd599811fb2377893479f
grantkee <grant@ether.ai>
2022-05-22 09:43:17 -0500

    update config trait

--- node-template/pallets/template/src/lib.rs
+++ node-template/pallets/template/src/lib.rs
@@ -1,37 +1,40 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-  pub use pallet::*;
+pub use pallet::*;
 
-  #[frame_support::pallet]
-  pub mod pallet {
-      use frame_support::pallet_prelude::*;
-      use frame_system::pallet_prelude::*;
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
 
-      // The struct on which we build all of our Pallet logic.
-      #[pallet::pallet]
-      #[pallet::generate_store(pub(super) trait Store)]
-      pub struct Pallet<T>(_);
+	// The struct on which we build all of our Pallet logic.
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
 
-      /* Placeholder for defining custom types. */
+	/* Placeholder for defining custom types. */
 
-      // TODO: Update the `config` block below
-      #[pallet::config]
-      pub trait Config: frame_system::Config {
-          type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
-      }
+	// TODO: Update the `config` block below
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// This pallet emits events and must depend on the runtime's definition of an event.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+		/// Constrain the maximum bytes of a hash used for any proof.
+		type MaxBytesInHash: Get<u32>;
+	}
 
-      // TODO: Update the `event` block below
-      #[pallet::event]
-      #[pallet::generate_deposit(pub(super) fn deposit_event)]
-      pub enum Event<T: Config> {}
+	// TODO: Update the `event` block below
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {}
 
-      // TODO: Update the `error` block below
-      #[pallet::error]
-      pub enum Error<T> {}
+	// TODO: Update the `error` block below
+	#[pallet::error]
+	pub enum Error<T> {}
 
-      // TODO: add #[pallet::storage] block
+	// TODO: add #[pallet::storage] block
 
-      // TODO: Update the `call` block below
-      #[pallet::call]
-      impl<T: Config> Pallet<T> {}
-  }
+	// TODO: Update the `call` block below
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {}
+}
