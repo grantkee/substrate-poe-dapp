8201c1621fe509ce4ac71b50d12f1cec68431540
grantkee <grant@ether.ai>
2022-05-22 09:53:44 -0500

    define two events for proof-of-existence: claim created/revoked

--- node-template/pallets/template/src/lib.rs
+++ node-template/pallets/template/src/lib.rs
@@ -14,7 +14,6 @@ pub mod pallet {
 
 	/* Placeholder for defining custom types. */
 
-	// TODO: Update the `config` block below
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		/// This pallet emits events and must depend on the runtime's definition of an event.
@@ -23,10 +22,17 @@ pub mod pallet {
 		type MaxBytesInHash: Get<u32>;
 	}
 
-	// TODO: Update the `event` block below
+	// Pallets use events to inform users when important changes are made.
+	// Event documentation should end with an array that provides descriptive 
+	// names for parameters. docs.substrate.io/v3/runtime/events-and-errors
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
-	pub enum Event<T: Config> {}
+	pub enum Event<T: Config> {
+		/// Event emitted when a proof is claimed. [who, claim]
+		ClaimCreated(T::AccountId, BoundedVec<u8, T::MaxBytesInHash>),
+		/// Event emitted when a claim is revocked by the owner. [who, claim]
+		ClaimRevoked(T::AccountId, BoundedVec<u8, T::MaxBytesInHash>),
+	}
 
 	// TODO: Update the `error` block below
 	#[pallet::error]
